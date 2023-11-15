b1c3a2cdca3750fcfeca64c892adb6f8a830183d
Ankit <ankitsharmgh093@gmail.com>
2023-11-15 18:45:01 +0530

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <Home />
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
